af7868e2c0651c493cb6ea59273ac2cd0d3e2294
Jasper <Jasper@192.168.2.1>
2017-04-19 16:00:48 +0200

    Begin

--- GHGEssentials/src/nl/gewoonhdgaming/ess/Main.java
+++ GHGEssentials/src/nl/gewoonhdgaming/ess/Main.java
@@ -4,6 +4,27 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
 	
+	static Main pl;
 	
+	@Override
+	public void onEnable() {
+		pl = this;
+		
+		this.getConfig().addDefault("Messages.prefix", "&f[&bGHG&6&lEssentials&f]");
+		
+		this.getConfig().options().copyDefaults(true);
+		
+		saveConfig();
+	}
+	
+	@Override
+	public void onDisable() {
+		saveConfig();
+	}
+	
+	public static Main getPlugin() {
+		return pl;
+	}
 
 }
+
